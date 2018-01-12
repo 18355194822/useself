bdd58d7fe25cf56783efb34049eb18cbca8536a6
18355194822 <34851317+18355194822@users.noreply.github.com>
2018-01-12 16:12:04 +0800

    22222

--- test/src/com/test.java
+++ test/src/com/test.java
@@ -3,11 +3,12 @@ package com;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("这是在本地添加的");
+		
 		long l = 3333L;
 		int i = (int)l;
 		System.out.println(l);
 		System.out.println(i);
+		System.out.println("这是在远程添加的");
 	}
 
 }
