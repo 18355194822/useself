de26ad81b061a0a92aca2f3358df475b193392d3
18355194822 <1046786437@qq.com>
2018-01-12 17:27:13 +0800

    首次提交到本地

---
+++ ttt/src/com/it/test.java
@@ -0,0 +1,13 @@
+package com.it;
+
+public class test {
+
+	public static void main(String[] args) {
+
+		long l = 3333L;
+		int i = (int)l;
+		System.out.println(l);
+		System.out.println(i);
+	}
+
+}
