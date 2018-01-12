473a18d423e4a672623f0bfaf51b1656e849c702
18355194822 <1046786437@qq.com>
2018-01-12 17:02:41 +0800

    3333

--- test/src/com/test.java
+++ test/src/com/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("========");
-
+		System.out.println("========");
 		long l = 3333L;
 		int i = (int)l;
 		System.out.println(l);
