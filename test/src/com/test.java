4f6a594bd106e1971f7d4411722bc448bd708df3
18355194822 <1046786437@qq.com>
2018-01-12 15:27:23 +0800

    增加输出信息

--- test/src/com/test.java
+++ test/src/com/test.java
@@ -4,7 +4,7 @@ public class test {
 
 	public static void main(String[] args) {
 		System.out.println("ffffffffff");
-		
+		System.out.println("guoqx");
 		long l = 3333L;
 		int i = (int)l;
 		System.out.println(l);
