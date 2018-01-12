8d285fa7aae08f9c41e76ea69d6a54ddf29c3299
18355194822 <1046786437@qq.com>
2018-01-12 17:10:58 +0800

    打印日志信息

--- test/src/com/test.java
+++ test/src/com/test.java
@@ -3,16 +3,11 @@ package com;
 public class test {
 
 	public static void main(String[] args) {
-		System.out.println("========");
-
-		System.out.println("========");
 
 		long l = 3333L;
 		int i = (int)l;
 		System.out.println(l);
 		System.out.println(i);
-		System.out.println("这是在远程添加的");
-		System.out.println("这是在远程添加的");
 	}
 
 }
